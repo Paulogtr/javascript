7d5f87ef3b207cccf90ac3154fa0c13e2159af12
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-07-13 20:03:24 -0300

    inicio de estudo de NodeJs

---
+++ Matheus_Battisti_Qualifica/modulo19_aprendendo_Nodejs/02_iniciando_projeto/index.js
@@ -0,0 +1,13 @@
+// Ferramenta utilizada para instalar módulos de JavaScript;
+// Que podem fazer desde operações matemáticas até comunicação HTTP;
+// Instalamos um pacote pelo comando: npm install pacote;
+// Vem junto com o Node;
+
+// Na última aula vimos que o node reclamou da falta do package.json;
+// O certo sempre é ter um arquivo desse ao iniciar um novo projeto;
+// Podemos criar a partir do template, utilizando o comando: npm init;
+// Aí teremos um projeto com o package.json;
+// Este arquivo serve para configurações e salva nossas dependências;
+
+//após utilizar o comando npm init , utilizamos o comando npm install express para poder instalar o pacote do express
+
